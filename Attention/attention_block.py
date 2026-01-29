2159ca1ff01c476f6538934857ea738f06423e57
git <josephawargo@gmail.com>
2026-01-28 22:48:28 -0500

    started understanding and working on LayerNorm

--- Attention/attention_block.py
+++ Attention/attention_block.py
@@ -8,7 +8,6 @@ class attention_block(object):
         self.input_dimension = input_dimension
         self.head_output_dimension = head_output_dimension
         self.block_output_dimension = block_output_dimension
-        # self.block_input_dimension = self.input_dimension * self.num_heads
 
         # dictionary to store heads
         self.heads = {}
@@ -20,24 +19,22 @@ class attention_block(object):
         # initialization of weights - using only Xavier for now
         xavier_val = np.sqrt(2/(self.input_dimension+self.block_output_dimension))
         self.W_o = np.random.normal(0, xavier_val, size=(self.input_dimension, self.block_output_dimension)).astype(np.float32)
-
-        # storing attention scores
-        self.attention_scores = {}
-        self.attention_scores_concat = None
         
-
     def multi_head_attention(self, word_embedding):
         # calculating the scores for each head
         # TODO: Parallelize
+        
+        attention_scores = {}
+
         for head_name in self.heads.keys():
             head = self.heads[head_name]
-            head_attention = head.attention_score(word_embedding)
-            self.attention_scores[head_name] = head_attention
+            head_attention = head.masked_attention_score(word_embedding)
+            attention_scores[head_name] = head_attention
 
         # concatenating scores together and running through the weights to get desired output shape
-        attention_scores_list = list(self.attention_scores.values())
+        attention_scores_list = list(attention_scores.values())
         head_attentions_concat = np.concatenate(attention_scores_list, axis=1)
 
         # saving and returning - not sure if we need to do both?
-        self.attention_scores_concat = head_attentions_concat @ self.W_o
-        return self.attention_scores_concat
+        attention_scores_concat = head_attentions_concat @ self.W_o
+        return attention_scores_concat
