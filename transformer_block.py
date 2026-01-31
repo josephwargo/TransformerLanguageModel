71a9a8f9dc3e3d9c819ea68800ed0b4c4bad2d0f
git <josephawargo@gmail.com>
2026-01-30 22:33:24 -0500

    Created transformer block forward pass in its own class!

--- transformer_block.py
+++ transformer_block.py
@@ -5,34 +5,58 @@ import layer_norm as ln
 
 class transformer_block(object):
     def __init__(
-            self, num_heads, input_dimension
+              self, num_heads, input_shape
+            , output_shape, activation
             , head_output_dimension
             , block_output_dimension
+            , batch_size, clip_val
+            , learning_rate , adam=False
             ):
+        
+        self.num_heads = num_heads
+        self.input_shape = input_shape
+        self.output_shape = output_shape
+        self.head_output_dimension = head_output_dimension
+        self.block_output_dimension = block_output_dimension
+        self.activation = activation
+        self.batch_size = batch_size
+        self.clip_val = clip_val
+        self.learning_rate = learning_rate
+        self.adam = adam
 
         # first layer norm
-        # TODO: update shapes to be hyperparameters
-        self.layer_norm_1 = ln.layer_norm(input_dimension)
+        self.layer_norm_1 = ln.layer_norm(input_shape)
 
         # self-attention
         self.self_attention = ab.attention_block(
-              num_heads=num_heads, input_dimension=input_dimension
+              num_heads=num_heads, input_dimension=input_shape
             , head_output_dimension=head_output_dimension
             , block_output_dimension=block_output_dimension
         )
-
-        # residual add
-        # TODO: remember to execute this during the forward pass
 
         # second layer norm
         self.layer_norm_2 = ln.layer_norm(block_output_dimension)
 
         # feed forward
-        # TODO: update shapes to be hyperparameters
-        feed_forward_layer = ff.neuron_layer(
-            input_shape=100, output_shape=100, activation='relu', batch_size=self.batch_size
-            , clip_val=self.clip_val, learning_rate=self.learning_rate, adam=self.adam
+
+        self.feed_forward_layer = ff.neuron_layer(
+              input_shape=input_shape, output_shape=output_shape, activation='relu'
+            , batch_size=self.batch_size, clip_val=self.clip_val, learning_rate=self.learning_rate
+            , adam=self.adam
         )
 
+
+    def forward_pass(self, x):
+        # first layer norm and attention masked self-attention
+        x_1 = self.self_attention.multi_head_attention(self.layer_norm_1.layer_norm(x))
+
         # residual add
-        # TODO: remember to execute this during the forward pass
+        x_2 = x_1 + x
+
+        # second layer norm & linear forward pass
+        x_3 = self.feed_forward_layer.forward_pass(self.layer_norm_2.layer_norm(x_2))
+
+        # residual add
+        x_4 = x_3 + x_1
+
+        return x_4
