Realized that my tensor shapes in forward_pass did not conform to the standards. Updating the forward pass to make sure it now does (currently, attention_head is outputting wrong dimension, so need to bug fix)

Attention/attention_head.py
@@ -15,6 +15,7 @@ class attention_head(object):
         xavier_val = np.sqrt(2/(self.dim_for_xavier))
 
         # instead of having separate heads, initiating each weight matrix with the shape (d_model, d_model), as this is mathematically equivalent to having separate heads
+        # does not matter whether in_dim or out_dim is first as both are d_model
         self.W_q = np.random.normal(0, xavier_val, size=(self.d_model, self.d_model)).astype(np.float32) # query weights
         self.W_k = np.random.normal(0, xavier_val, size=(self.d_model, self.d_model)).astype(np.float32) # key weights
         self.W_v = np.random.normal(0, xavier_val, size=(self.d_model, self.d_model)).astype(np.float32) # value weights
@@ -30,11 +31,13 @@ class attention_head(object):
     def calculate_q_k_v(self, x):
         # reshaping the output to be (batch, seq_len, num_heads, head_shape) as this is needed to calculate the attention score for different heads
         # will later reshape to (batch, seq_len, d_model) when we "concatenate" the separate heads in the attention block
-        self.q = (x @ self.W_q).reshape(x.shape[0], self.num_heads, x.shape[1], self.head_dimension)
-        self.k = (x @ self.W_k).reshape(x.shape[0], self.num_heads, x.shape[1], self.head_dimension)
-        self.v = (x @ self.W_v).reshape(x.shape[0], self.num_heads, x.shape[1], self.head_dimension)
-
-        # return q, k, v
+        # doing the reshaping in two steps to avoid memory bug
+        q_proj = (x @ self.W_q).reshape(x.shape[0], x.shape[1], self.num_heads, self.head_dimension)
+        self.q = q_proj.transpose(0,1,2,3)
+        k_proj = (x @ self.W_k).reshape(x.shape[0], x.shape[1], self.num_heads, self.head_dimension)
+        self.k = k_proj.transpose(0,1,2,3)
+        v_proj = (x @ self.W_v).reshape(x.shape[0], x.shape[1], self.num_heads, self.head_dimension)
+        self.v = v_proj.transpose(0,1,2,3)
 
     def forward_pass(self, x, train=False):
         # calculating q, k, and v
@@ -62,9 +65,11 @@ class attention_head(object):
     
     def backward_pass(self, dL_dY):
         dL_dv = self.softmax_masked_score @ dL_dY
+        print(dL_dv.shape)
         # gradient w.r.t. softmax_masked_scores
         print("here")
+        # print(self.softmax_masked_score.shape)
         print(self.v.shape)
-        print(dL_dY.shape)
+        # print(dL_dY.shape)
         dL_dY = self.v @ dL_dY
-        print(dL_dY.shape)
+        # print(dL_dY.shape)