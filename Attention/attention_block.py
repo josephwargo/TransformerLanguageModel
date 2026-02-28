Realized that my tensor shapes in forward_pass did not conform to the standards. Updating the forward pass to make sure it now does (currently, attention_head is outputting wrong dimension, so need to bug fix)

Attention/attention_block.py
@@ -21,7 +21,7 @@ class attention_block(object):
         # weights to aggregate heads
         # initialization of weights - using only Xavier for now
         xavier_val = np.sqrt(2/(self.d_model+self.d_model))
-        self.W_o = np.random.normal(0, xavier_val, size=(self.d_model, self.d_model)).astype(np.float32)
+        self.W_o = np.random.normal(0, xavier_val, size=(self.d_model, self.d_model)).astype(np.float32) # does not matter whether in_dim or out_dim is first as both are d_model
 
         # hidden state
         self.hidden_state = None
@@ -34,7 +34,10 @@ class attention_block(object):
 
         # concatenating scores together and running through the weights to get desired output shape
         # concatenating by reshaping from (batch, seq_len, num_heads, head_shape) to (batch, seq_len, d_model)
-        head_attentions_concat = head_attention.reshape(x.shape[0], x.shape[1], self.d_model)
+        print(head_attention.shape)
+        # need to transpose so that the dimensions I am trying to combine ()
+        head_attention_transposed = head_attention.transpose(0,2,1,3)
+        head_attentions_concat = head_attention_transposed.reshape(x.shape[0], x.shape[1], self.d_model)
 
         # calculating final attention scores and returning
         attention_scores_concat = head_attentions_concat @ self.W_o

feed_forward.py
@@ -10,7 +10,8 @@ class neuron_layer(object):
         self.input_shape = input_shape # length of vector that will be inputted into the weights
         self.output_shape = output_shape # desired length of weight output (and therefore layer output)
         layer_xavier = np.sqrt(2/(self.input_shape+self.output_shape)) # value used to determine optimal
-        self.layer_weights = np.random.normal(0,layer_xavier, size=(self.input_shape,self.output_shape)).astype(np.float32) # optimal weight initialization via xavier
+        # shape is (out_dim, in_dim) for hardware optimization. this is NOT untuitive
+        self.layer_weights = np.random.normal(0,layer_xavier, size=(self.output_shape, self.input_shape)).astype(np.float32)
 
         self.bias = np.zeros(shape=(output_shape)).astype(np.float32) # initializing bias as 0s - no xavier here
 
@@ -27,6 +28,7 @@ class neuron_layer(object):
         self.prev_layer_hidden_state = None
 
         # adam
+        # TODO: implement
         self.adam = adam
         if adam:
             # constants
@@ -47,9 +49,9 @@ class neuron_layer(object):
 ####################################
     def forward_pass(self, x, train=False):
         if self.activation==None:
-            hidden_state = x @ self.layer_weights + self.bias
+            hidden_state = x @ self.layer_weights.T + self.bias
         else:
-            hidden_state = caa.activation(self.activation, x @ self.layer_weights + self.bias)
+            hidden_state = caa.activation(self.activation, x @ self.layer_weights.T + self.bias)
         if train:
             self.hidden_state = hidden_state
             self.prev_layer_hidden_state = x