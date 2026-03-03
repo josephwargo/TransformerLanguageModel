9cda3d9d1e0b15ed42d576c6ec0ce0235eac58a5
git <josephawargo@gmail.com>
2026-03-02 23:04:34 -0500

    Made misc. work on the backward pass. Also started building out documentation where I go step-by-step through the forward pass.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-Context: I am coding my way through deep learning history using just numpy to improve my understanding of AI
+I am coding my way through deep learning history using just numpy to improve my understanding of AI. This is a WIP implementation of a Transformer language model
 
-This is a WIP implementation of a Transformer language model
+I am using this doc to detail each step of the forward and backward pass to make sure I am truly understanding what I am building: https://docs.google.com/spreadsheets/d/1bDB1wPxcVrq55hjSDb3glXPYFl0f3-6sNImfGcF9huw/edit?usp=sharing
--- layer_norm.py
+++ layer_norm.py
@@ -5,13 +5,42 @@ class layer_norm(object):
         self.gamma = np.ones(shape=input_size)
         self.beta = np.zeros(shape=input_size)
 
-    def x_hat(self, x):
-        x_mean = np.mean(x)
-        x_std_dev = np.std(x)
+        self.prev_layer_hidden_state = None
+        self.x_std_dev = None
+        self.x_hat_val = None
 
-        x_hat = (x - x_mean) / np.sqrt(x_std_dev + 1e-5)
+    def x_hat(self, x, train=False):
+        if train:
+            self.prev_layer_hidden_state = x
+
+        x_mean = np.mean(x, axis=-1, keepdims=True)
+        x_std_dev = np.sqrt(np.var(x, axis=-1, keepdims=True) + 1e-5)
+
+        if train:
+            self.x_std_dev = x_std_dev
+
+        x_hat = (x - x_mean) / x_std_dev
 
         return x_hat
     
     def forward_pass(self, x, train=False):
-        return self.gamma * self.x_hat(x) + self.beta
+        
+        self.x_hat_val = self.x_hat(x, train)
+        
+        layer_normed = self.gamma * self.x_hat_val + self.beta
+        
+        return layer_normed
+    
+    def backward_pass(self, dL_dY):
+        
+        # gradients to update gamma and beta
+        dL_dgamma = (dL_dY.transpose(0,2,1) @ self.x_hat_val).sum(axis=(0,1))
+        dL_dgamma = (dL_dY * self.x_hat_val).sum(axis=(0,1))
+        dL_dbeta = dL_dY.sum(axis=(0,1))
+
+        # gradient to pass back into normalization step
+        dL_dY = dL_dY * self.gamma
+
+        # passing gradient back through normalization
+        # TODO: write this
+        # dL_dY = dL_dY / self.x_std_dev
